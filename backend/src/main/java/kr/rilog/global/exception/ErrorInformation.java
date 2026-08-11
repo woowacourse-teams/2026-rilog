3c04de7af237ad4f372944eb6cfb70b204f9a41d
koreaioi <koreaioi@gachon.ac.kr>
2026-08-12 01:29:50 +0900

    feat: ErrorInformation 추가 (#19)
    - 커스텀 예외 생성자 시그니처를 Interface로 단축

---
+++ backend/src/main/java/kr/rilog/global/exception/ErrorInformation.java
@@ -0,0 +1,9 @@
+package kr.rilog.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorInformation {
+    HttpStatus getHttpStatus();
+    String getErrorCode();
+    String getMessage();
+}
