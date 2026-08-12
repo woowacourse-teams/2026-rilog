c4bd326f910f6f43cda758f51c8833a049b7d853
koreaioi <koreaioi@gachon.ac.kr>
2026-08-12 15:57:07 +0900

    refactor: HealthCheckController 패키지 이동 (#34)

--- backend/src/main/java/kr/rilog/global/healthcheck/HealthCheckController.java
+++ backend/src/main/java/kr/rilog/global/healthcheck/HealthCheckController.java
@@ -1,10 +1,10 @@
-package kr.rilog.controller;
+package kr.rilog.global.healthcheck;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HealthController {
+public class HealthCheckController {
 
 	@GetMapping("/health")
 	public String health() {
