1778e1d7385f05a6de8dc8c62ec54d6412a1640c
koreaioi <koreaioi@gachon.ac.kr>
2026-08-12 13:45:53 +0900

    feat: 임시 LoginUserSlugArgumentResolver 추가 (#31)
    - 작업 병목을 막기 위한 임시 ArgumentResolver

---
+++ backend/src/main/java/kr/rilog/global/auth/resolver/LoginUserSlugArgumentResolver.java
@@ -0,0 +1,34 @@
+package kr.rilog.global.auth.resolver;
+
+import kr.rilog.global.auth.annotation.LoginUserId;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class LoginUserSlugArgumentResolver implements HandlerMethodArgumentResolver{
+
+    private static final String USER_SLUG_HEADER = "X-Test-User-Slug";
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(LoginUserId.class)
+                && parameter.getParameterType() == String.class;
+    }
+
+    // TODO 토큰 구현 후 실제 로직으로 변경할 것.
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        String value = webRequest.getHeader(USER_SLUG_HEADER);
+
+        if (value == null || value.isBlank()) {
+            return "testSlug";
+        }
+
+        return value;
+    }
+
+}
