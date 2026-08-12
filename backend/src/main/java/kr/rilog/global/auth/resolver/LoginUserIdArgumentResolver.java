e3f3337629984296a8a6ce8bdcb9d732789cb184
koreaioi <koreaioi@gachon.ac.kr>
2026-08-12 15:06:07 +0900

    [Feature] #31 로그인 사용자 식별자 초기 구현 (#32)
    * feat: @LoginUserId 추가 (#31)
    
    * feat: @LoginUserSlug 추가 (#31)
    
    * feat: 임시 LoginUserIdArgumentResolver 추가 (#31)
    
    - 작업 병목을 막기 위한 임시 ArgumentResolver
    
    * feat: 임시 LoginUserSlugArgumentResolver 추가 (#31)
    
    - 작업 병목을 막기 위한 임시 ArgumentResolver

---
+++ backend/src/main/java/kr/rilog/global/auth/resolver/LoginUserIdArgumentResolver.java
@@ -0,0 +1,41 @@
+package kr.rilog.global.auth.resolver;
+
+import jakarta.servlet.http.HttpServletRequest;
+import kr.rilog.global.auth.annotation.LoginUserId;
+import org.springframework.core.MethodParameter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class LoginUserIdArgumentResolver implements HandlerMethodArgumentResolver{
+
+    private static final String USER_ID_HEADER = "X-Test-User-Id";
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(LoginUserId.class)
+                && (parameter.getParameterType() == Long.class
+                || parameter.getParameterType() == long.class);
+    }
+
+    // TODO 토큰 구현 후 실제 로직으로 변경할 것.
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        String value = webRequest.getHeader(USER_ID_HEADER);
+
+        if (value == null || value.isBlank()) {
+            return 1L;
+        }
+
+        try {
+            return Long.valueOf(value);
+        } catch (NumberFormatException exception) {
+            return 1L;
+        }
+    }
+
+}
