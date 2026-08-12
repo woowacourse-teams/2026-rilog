e6ebe04013b9e7bdfa34a81a627f37fe4aedbc20
koreaioi <koreaioi@gachon.ac.kr>
2026-08-13 01:15:17 +0900

    feat: PostService 추가 및 게시글 작성 구현
    - 이미지 처리는 다음 이슈에 구현

---
+++ backend/src/main/java/kr/rilog/domain/post/service/PostService.java
@@ -0,0 +1,67 @@
+package kr.rilog.domain.post.service;
+
+import kr.rilog.domain.blog.entity.Blog;
+import kr.rilog.domain.blog.exception.BlogException;
+import kr.rilog.domain.blog.repository.BlogRepository;
+import kr.rilog.domain.post.entity.Post;
+import kr.rilog.domain.post.repository.PostRepository;
+import kr.rilog.domain.post.service.dto.command.PostSaveCommand;
+import kr.rilog.domain.post.service.dto.result.PostPublishResult;
+import kr.rilog.domain.user.entity.User;
+import kr.rilog.domain.user.exception.UserException;
+import kr.rilog.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static kr.rilog.domain.blog.exception.BlogErrorInformation.BLOG_NOT_FOUND;
+import static kr.rilog.domain.blog.exception.BlogErrorInformation.RILOG_NOT_FOUND;
+import static kr.rilog.domain.user.exception.UserErrorInformation.USER_NOT_FOUND;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+    private final BlogRepository blogRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public PostPublishResult publish(PostSaveCommand command, Long blogId, Long requesterId) {
+        Blog publishingBlog = getBlog(blogId);
+        User writer = getUser(requesterId);
+
+        Post post = publishingBlog.isColog()
+                ? publishToColog(command, publishingBlog, writer)
+                : publishToRilog(command, publishingBlog, writer);
+
+        // TODO content에 있는 이미지 파싱하여, S3에 tag 변경...! 이건 그냥 주석으로 유지 다음에 구현..!
+        Post published = postRepository.save(post);
+        return PostPublishResult.of(published, publishingBlog);
+    }
+
+    private Post publishToRilog(PostSaveCommand command, Blog rilog, User writer) {
+        return Post.create(rilog, writer, command.toDetail());
+    }
+
+    private Post publishToColog(PostSaveCommand command, Blog colog, User writer) {
+        Blog rilog = getRilog(writer);
+        return Post.create(colog, rilog, writer, command.toDetail());
+    }
+
+    private User getUser(Long requesterId) {
+        return userRepository.findById(requesterId)
+                .orElseThrow(() -> new UserException(USER_NOT_FOUND));
+    }
+
+    private Blog getBlog(Long blogId) {
+        return blogRepository.findById(blogId)
+                .orElseThrow(() -> new BlogException(BLOG_NOT_FOUND));
+    }
+
+    private Blog getRilog(User writer) {
+        return blogRepository.findRilogByOwnerId(writer.getId())
+                .orElseThrow(() -> new BlogException(RILOG_NOT_FOUND));
+    }
+
+}
