6ecf124c64f93449d129150598f2e05f9858554f
koreaioi <koreaioi@gachon.ac.kr>
2026-08-12 10:39:33 +0900

    refactor: 클래스 선언 직후, 괄호 닫기 전 개행 추가 (#19)

--- backend/src/main/java/kr/rilog/global/exception/ErrorInformation.java
+++ backend/src/main/java/kr/rilog/global/exception/ErrorInformation.java
@@ -3,7 +3,11 @@ package kr.rilog.global.exception;
 import org.springframework.http.HttpStatus;
 
 public interface ErrorInformation {
+
     HttpStatus getHttpStatus();
+
     String getErrorCode();
+
     String getMessage();
+
 }
