9cd986c536a74f1e18e5ad51f20048d6eec14a05
koreaioi <koreaioi@gachon.ac.kr>
2026-08-13 00:58:58 +0900

    feat: BlogRepository 추가
    - 개인사용자의 블로그를 조회하는 메서드 추가

---
+++ backend/src/main/java/kr/rilog/domain/blog/repository/BlogRepository.java
@@ -0,0 +1,22 @@
+package kr.rilog.domain.blog.repository;
+
+import kr.rilog.domain.blog.entity.Blog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Blog, Long> {
+
+    @Query("""
+            SELECT blog
+            FROM Blog blog
+            WHERE blog.owner.id = :ownerId
+              AND blog.blogType = kr.rilog.domain.blog.entity.enums.BlogType.RILOG
+            """)
+    Optional<Blog> findRilogByOwnerId(@Param("ownerId") Long ownerId);
+
+}
