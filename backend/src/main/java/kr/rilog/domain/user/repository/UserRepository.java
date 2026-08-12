11326b9a2ca6437f4c00f48542f034086ec4de9a
koreaioi <koreaioi@gachon.ac.kr>
2026-08-13 00:58:13 +0900

    feat: UserRepository 추가

---
+++ backend/src/main/java/kr/rilog/domain/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package kr.rilog.domain.user.repository;
+
+import kr.rilog.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
