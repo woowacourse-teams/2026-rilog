c02f6f386b1f2b051976a0cee3658da5408ac6cf
koreaioi <koreaioi@gachon.ac.kr>
2026-08-13 17:31:12 +0900

    feature: 전체피드 게시글 목록 조회 API 추가

---
+++ backend/src/main/java/kr/rilog/domain/post/controller/FeedController.java
@@ -0,0 +1,28 @@
+package kr.rilog.domain.post.controller;
+
+import kr.rilog.domain.post.controller.dto.response.FullFeedPostResponse;
+import kr.rilog.domain.post.service.FeedService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+@RequiredArgsConstructor
+public class FeedController {
+
+    private final FeedService feedService;
+
+    @GetMapping("/feeds/posts")
+    public ResponseEntity<FullFeedPostResponse> readFullFeedPosts(
+            @RequestParam int page,
+            @RequestParam int size
+    ) {
+        FullFeedPostResponse data = feedService.getFullFeedPostList(page, size);
+        return ResponseEntity.ok(data);
+    }
+
+}
