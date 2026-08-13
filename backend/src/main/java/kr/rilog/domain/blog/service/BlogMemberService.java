ed1926136c060939c08d5e63119d0edeef39dd58
koreaioi <koreaioi@gachon.ac.kr>
2026-08-13 22:46:34 +0900

    feat: 팀목록 조회 비즈니스 로직 추가

---
+++ backend/src/main/java/kr/rilog/domain/blog/service/BlogMemberService.java
@@ -0,0 +1,40 @@
+package kr.rilog.domain.blog.service;
+
+import kr.rilog.domain.blog.entity.Blog;
+import kr.rilog.domain.blog.entity.BlogMember;
+import kr.rilog.domain.blog.entity.enums.BlogMemberStatus;
+import kr.rilog.domain.blog.entity.enums.BlogType;
+import kr.rilog.domain.blog.exception.BlogException;
+import kr.rilog.domain.blog.repository.BlogMemberRepository;
+import kr.rilog.domain.blog.repository.BlogRepository;
+import kr.rilog.domain.blog.service.dto.result.BlogMemberResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static kr.rilog.domain.blog.exception.BlogErrorInformation.BLOG_NOT_FOUND;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class BlogMemberService {
+
+    private final BlogRepository blogRepository;
+    private final BlogMemberRepository blogMemberRepository;
+
+    public List<BlogMemberResult> getCologMembers(String slug) {
+        Blog colog = getColog(slug);
+
+        return blogMemberRepository.findAllWithUserByBlogIdAndStatus(colog.getId(), BlogMemberStatus.ACTIVE).stream()
+                .map(BlogMemberResult::from)
+                .toList();
+    }
+
+    private Blog getColog(String slug) {
+        return blogRepository.findBySlugAndBlogType(slug, BlogType.COLOG)
+                .orElseThrow(() -> new BlogException(BLOG_NOT_FOUND));
+    }
+
+}
