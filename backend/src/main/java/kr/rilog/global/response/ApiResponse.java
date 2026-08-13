4414d1d02edd5bf1bdc3805994d0d7c0f052650d
koreaioi <koreaioi@gachon.ac.kr>
2026-08-13 18:24:10 +0900

    [Refactor] #80 공통 응답 형식 적용 (#82)
    * refactor: 공통 응답 ApiResponse 적용
    
    - 소셜 로그인 컨트롤러는 미적용
    
    * feature: ApiResponse 응답 적용
    
    * refactor: 공통 응답 ApiResponse 적용
    
    - 소셜 로그인 컨트롤러는 미적용
    
    * feat: .gradle-user-home/ .gitignore 추가

---
+++ backend/src/main/java/kr/rilog/global/response/ApiResponse.java
@@ -0,0 +1,39 @@
+package kr.rilog.global.response;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import org.springframework.http.HttpStatus;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record ApiResponse<T>(
+        int status,
+        String code,
+        String message,
+        T data
+) {
+
+    public static <T> ApiResponse<T> response(HttpStatus httpStatus, String message, T data) {
+        return ApiResponse.<T>builder()
+                .status(httpStatus.value())
+                .message(message)
+                .data(data)
+                .build();
+    }
+
+    public static <T> ApiResponse<T> response(HttpStatus httpStatus, String message) {
+        return ApiResponse.<T>builder()
+                .status(httpStatus.value())
+                .message(message)
+                .build();
+    }
+
+    public static <T> ApiResponse<T> response(HttpStatus httpStatus, String code, String message, T data) {
+        return ApiResponse.<T>builder()
+                .status(httpStatus.value())
+                .code(code)
+                .message(message)
+                .data(data)
+                .build();
+    }
+
+}
