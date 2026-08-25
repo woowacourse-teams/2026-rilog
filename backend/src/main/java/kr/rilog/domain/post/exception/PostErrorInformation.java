150316f528b617fa764370496a845ab7798e22b4
koreaioi <koreaioi@gachon.ac.kr>
2026-08-25 14:19:19 +0900

    refactor: 에러코드 400로 수정

--- backend/src/main/java/kr/rilog/domain/post/exception/PostErrorInformation.java
+++ backend/src/main/java/kr/rilog/domain/post/exception/PostErrorInformation.java
@@ -11,7 +11,7 @@ public enum PostErrorInformation implements ErrorInformation {
 
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 게시글을 찾을 수 없습니다."),
     PRIVATE_POST_READ_FORBIDDEN(HttpStatus.FORBIDDEN, "비공개 게시글 조회 권한이 없습니다."),
-    INVALID_POST_CONTENT(HttpStatus.NOT_FOUND, "게시글 본문 내용이 올바르지 않습니다.")
+    INVALID_POST_CONTENT(HttpStatus.BAD_REQUEST, "게시글 본문 내용이 올바르지 않습니다.")
     ;
 
     private final HttpStatus httpStatus;
