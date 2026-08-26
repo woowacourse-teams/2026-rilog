0b7b458c2e69cfae20bdff0bec25bc5ab7b9b460
koreaioi <koreaioi@gachon.ac.kr>
2026-08-26 18:24:11 +0900

    feature: Draft 목록 조회 무한스크롤 API 추가

--- backend/src/main/java/kr/rilog/domain/post/controller/DraftController.java
+++ backend/src/main/java/kr/rilog/domain/post/controller/DraftController.java
@@ -5,9 +5,11 @@ import kr.rilog.domain.auth.annotation.AuthGuard;
 import kr.rilog.domain.auth.annotation.LoginUserId;
 import kr.rilog.domain.post.controller.apispec.DraftApiSpec;
 import kr.rilog.domain.post.controller.dto.response.DraftIdResponse;
+import kr.rilog.domain.post.controller.dto.response.DraftListResponse;
 import kr.rilog.domain.post.service.DraftService;
 import kr.rilog.domain.post.service.dto.command.DraftSaveCommand;
 import kr.rilog.domain.post.service.dto.result.DraftIdResult;
+import kr.rilog.domain.post.service.dto.result.DraftListResult;
 import kr.rilog.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -30,6 +32,18 @@ public class DraftController implements DraftApiSpec {
         DraftIdResult result = draftService.saveDraft(command, userId);
         DraftIdResponse data = DraftIdResponse.from(result);
         return ApiResponse.response(HttpStatus.CREATED, "최초 임시저장에 성공했습니다.", data);
+    }
+
+    @AuthGuard
+    @GetMapping("/drafts/me")
+    public ApiResponse<DraftListResponse> readMyDraftList(
+            @LoginUserId Long requesterId,
+            @RequestParam int page,
+            @RequestParam int size
+    ) {
+        DraftListResult result = draftService.readMyDraftList(requesterId, page, size);
+        DraftListResponse data = DraftListResponse.from(result);
+        return ApiResponse.response(HttpStatus.OK, "임시저장 목록 조회에 성공했습니다.", data);
     }
 
 }
