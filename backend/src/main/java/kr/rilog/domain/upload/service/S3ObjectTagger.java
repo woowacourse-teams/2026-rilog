1f68972d3f7ece517e2a07cc244093bcb6c20e9f
koreaioi <koreaioi@gachon.ac.kr>
2026-08-26 11:04:57 +0900

    refactor: 버킷 검증을 Tagger로 이동

--- backend/src/main/java/kr/rilog/domain/upload/service/S3ObjectTagger.java
+++ backend/src/main/java/kr/rilog/domain/upload/service/S3ObjectTagger.java
@@ -2,6 +2,7 @@ package kr.rilog.domain.upload.service;
 
 import kr.rilog.domain.upload.domain.TagStatus;
 import kr.rilog.domain.upload.domain.vo.S3ObjectAddress;
+import kr.rilog.domain.upload.exception.UploadException;
 import kr.rilog.global.s3.properties.S3Properties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,8 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
 
 import java.util.List;
+
+import static kr.rilog.domain.upload.exception.UploadErrorInformation.UNSUPPORTED_S3_BUCKET;
 
 @Slf4j
 @Service
@@ -21,7 +24,8 @@ public class S3ObjectTagger {
 
     public void fileTag(List<String> objectUrls, TagStatus tagStatus) {
         for (String objectUrl : objectUrls) {
-            S3ObjectAddress address = S3ObjectAddress.from(objectUrl, properties.bucket());
+            S3ObjectAddress address = S3ObjectAddress.from(objectUrl);
+            validateBucket(address.bucket(), properties.bucket());
             changeS3ObjectTag(address.key(), tagStatus);
         }
     }
@@ -32,6 +36,12 @@ public class S3ObjectTagger {
                 .key(key)
                 .tagging(tagStatus.toTagging())
                 .build());
+    }
+
+    private void validateBucket(String parsedBucket, String originBucket) {
+        if (!parsedBucket.equals(originBucket)) {
+            throw new UploadException(UNSUPPORTED_S3_BUCKET);
+        }
     }
 
 }
