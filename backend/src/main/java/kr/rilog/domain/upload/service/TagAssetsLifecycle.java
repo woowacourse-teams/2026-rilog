37989b39e708d370453f6d32437452410fcedaba
koreaioi <koreaioi@gachon.ac.kr>
2026-08-27 17:39:20 +0900

    docs: TagAssetsLifecycle java docs 추가

--- backend/src/main/java/kr/rilog/domain/upload/service/TagAssetsLifecycle.java
+++ backend/src/main/java/kr/rilog/domain/upload/service/TagAssetsLifecycle.java
@@ -4,10 +4,12 @@ import kr.rilog.domain.upload.domain.vo.TagAssets;
 
 public interface TagAssetsLifecycle {
 
+    /** TEMPORARY -> CONFIRMED*/
     void attach(TagAssets assets);
 
     void synchronize(TagAssets previous, TagAssets current);
 
+    /** CONFIRMED -> TEMPORARY */
     void detach(TagAssets assets);
 
 }
