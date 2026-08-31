a7a3837ccac5cef13f09ed3044e8b7450245435c
koreaioi <koreaioi@gachon.ac.kr>
2026-08-31 09:59:38 +0900

    refactor: requestDto 챕터 이름 25자로 수정

--- backend/src/main/java/kr/rilog/domain/chapter/controller/dto/request/ChapterCreateRequest.java
+++ backend/src/main/java/kr/rilog/domain/chapter/controller/dto/request/ChapterCreateRequest.java
@@ -7,7 +7,7 @@ import kr.rilog.domain.chapter.service.dto.command.ChapterCreateCommand;
 public record ChapterCreateRequest(
 
         @NotBlank(message = "챕터 이름은 필수입니다.")
-        @Size(max = 20, message = "챕터 이름은 20자 이하여야 합니다.")
+        @Size(max = 25, message = "챕터 이름은 20자 이하여야 합니다.")
         String name
 
 ) {
--- backend/src/main/java/kr/rilog/domain/chapter/controller/dto/request/ChapterRenameRequest.java
+++ backend/src/main/java/kr/rilog/domain/chapter/controller/dto/request/ChapterRenameRequest.java
@@ -7,7 +7,7 @@ import kr.rilog.domain.chapter.service.dto.command.ChapterRenameCommand;
 public record ChapterRenameRequest(
 
         @NotBlank(message = "챕터 이름은 필수입니다.")
-        @Size(max = 20, message = "챕터 이름은 20자 이하여야 합니다.")
+        @Size(max = 25, message = "챕터 이름은 20자 이하여야 합니다.")
         String name
 
 ) {
