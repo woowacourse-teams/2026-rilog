3a35261159a76a2d82641217508dc32f421b5050
koreaioi <koreaioi@gachon.ac.kr>
2026-09-03 18:48:51 +0900

    feature: TagAssetsListener 추가
    - 트랜잭션 커밋 이후, 동작하도록 phase AFTER_COMMIT으로 설정 (default이지만 명시)
    - 별도 스레드가 이벤트를 수행하도록 @Async 적용

---
+++ backend/src/main/java/kr/rilog/domain/upload/listener/TagAssetsListener.java
@@ -0,0 +1,43 @@
+package kr.rilog.domain.upload.listener;
+
+import kr.rilog.domain.upload.event.TagAssetsEvent;
+import kr.rilog.domain.upload.service.S3TagAssetsLifecycle;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import static kr.rilog.global.config.AsyncConfig.S3_TAGGING_EXECUTOR;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TagAssetsListener {
+
+    private final S3TagAssetsLifecycle lifecycle;
+
+//    @Async(S3_TAGGING_EXECUTOR)
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handle(TagAssetsEvent.Attach event) {
+        log.info("TAG ATTACH LISTENER 수행");
+        log.info(event.toString());
+        lifecycle.attach(event.assets());
+    }
+
+    @Async(S3_TAGGING_EXECUTOR)
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handle(TagAssetsEvent.Synchronize event) {
+        log.info("TAG SYNCHRONIZE LISTENER 수행");
+        log.info(event.toString());
+        lifecycle.synchronize(event.previous(), event.current());
+    }
+
+    @Async(S3_TAGGING_EXECUTOR)
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handle(TagAssetsEvent.Detach event) {
+        lifecycle.detach(event.assets());
+    }
+
+}
