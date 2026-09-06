7cb02038c8a7fc24c61daed8234fa48c49ce7938
JetProc <46674347+JetProc@users.noreply.github.com>
2026-09-06 15:32:49 +0900

    refactor: 불필요한 webpack 설정 제거 (#468)

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from 'next';
 
-interface WebpackConfigWithRules {
-	module: {
-		rules: unknown[];
-	};
-}
-
 const nextConfig: NextConfig = {
 	agentRules: false,
 	serverExternalPackages: ['@blocknote/core', '@blocknote/react', '@blocknote/server-util'],
@@ -46,21 +40,6 @@ const nextConfig: NextConfig = {
 				as: '*.js',
 			},
 		},
-	},
-	webpack(config: WebpackConfigWithRules) {
-		config.module.rules.push({
-			test: /\.svg$/i,
-			use: [
-				{
-					loader: '@svgr/webpack',
-					options: {
-						icon: true,
-					},
-				},
-			],
-		});
-
-		return config;
 	},
 };
 
